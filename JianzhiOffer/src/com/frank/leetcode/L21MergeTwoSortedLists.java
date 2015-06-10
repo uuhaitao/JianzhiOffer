5900c4ab8423889c5830e00d708bef49b78d4f01
uuhaitao <895739909@qq.com>
2015-06-10 15:03:16 +0800

    add L21

---
+++ JianzhiOffer/src/com/frank/leetcode/L21MergeTwoSortedLists.java
@@ -0,0 +1,52 @@
+package com.frank.leetcode;
+
+/*
+ * Merge two sorted linked lists and return it as a new list. 
+ * The new list should be made by splicing together the nodes of the first two lists.
+ */
+
+public class L21MergeTwoSortedLists {
+
+	class ListNode {
+		int val;
+		ListNode next;
+
+		ListNode(int x) {
+			val = x;
+		}
+	}
+
+	public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+		if (l1 == null)
+			return l2;
+		if (l2 == null)
+			return l1;
+
+		ListNode la = l1;
+		ListNode lb = l2;
+
+		ListNode curr = new ListNode(0);
+		ListNode res = curr;
+
+		while (la != null && lb != null) {
+
+			if (la.val < lb.val) {
+				curr.next = la;
+				curr = la;
+				la = la.next;
+			} else {
+				curr.next = lb;
+				curr = lb;
+				lb = lb.next;
+			}
+		}
+		if(la!=null)
+			curr.next=la;
+		if(lb!=null)
+			curr.next=lb;
+		
+		
+		return res.next;
+	}
+}
