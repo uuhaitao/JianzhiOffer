4ae0e97bd093dc3a64a5ecf773c313bae60c963f
uuhaitao <895739909@qq.com>
2015-06-10 16:36:52 +0800

    add L22

---
+++ JianzhiOffer/src/com/frank/leetcode/L22GenerateParentheses.java
@@ -0,0 +1,43 @@
+package com.frank.leetcode;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class L22GenerateParentheses {
+
+	public List<String> generateParenthesis(int n) {
+
+		ArrayList<String> res = new ArrayList<String>();
+		generate_core(0,0, n, "", res);
+		return res;
+	}
+
+	void generate_core(int left, int right, int n,String curr, ArrayList<String> res) {
+		if (left == n && right == n) {
+			res.add(curr);
+		}
+		if(left>right){
+			if(left<n){
+				generate_core(left+1,right,n,curr+"(",res);
+			}
+			if(right<n){
+				generate_core(left,right+1,n,curr+")",res);
+			}
+		}
+		else{
+			if(left<n){
+				generate_core(left+1,right,n,curr+"(",res);
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		List<String> r=new L22GenerateParentheses().generateParenthesis(3);
+		for (String s:r){
+			System.out.println(s);
+		}
+	}
+
+}
